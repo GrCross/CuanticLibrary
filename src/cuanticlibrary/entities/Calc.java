815d34e30207f88b321bfcc37bd6594ffac0200b
daniel <rosalesdaniel350@gmail.com>
2019-08-09 12:57:48 -0500

    Multipicacion

--- src/cuanticlibrary/entities/Calc.java
+++ src/cuanticlibrary/entities/Calc.java
@@ -5,11 +5,17 @@
  */
 package cuanticlibrary.entities;
 
+
+
 /**
  *
  * @author daniel
  */
 public class Calc{
+    
+    private double[] firstComplex = null;
+    private double[] secondComplex = null;
+           
 
     public Calc(){
     }
@@ -21,10 +27,38 @@ public class Calc{
     }
 
     public Complex mult(Complex first, Complex second){
-        double[] firstA = {first.getRealP(),first.getImagiP()};
-        double[] secondA = {second.getRealP(),second.getImagiP()};
-        double multReal = firstA[0]*secondA[0]-firstA[1]*secondA[1];
-        double multImagi = firstA[0]*secondA[1]+firstA[1]*secondA[0];
+        extractComponents(first, second);
+        double multReal = firstComplex[0]*secondComplex[0]-firstComplex[1]*secondComplex[1];
+        double multImagi = firstComplex[0]*secondComplex[1]+firstComplex[1]*secondComplex[0];
         return new Complex(multReal, multImagi);
+    }
+    
+    public Complex substract(Complex first, Complex second){
+        
+        extractComponents(first, second);
+        double subsReal = firstComplex[0] - secondComplex[0];
+        double subsImagi = firstComplex[1] - secondComplex[1];
+        return new Complex(subsReal,subsImagi);
+    }
+    
+    public Complex division(Complex first, Complex second ){
+        extractComponents(first, second);
+        double realNumerator = firstComplex[0]*secondComplex[0]+firstComplex[1]*secondComplex[1];
+        double realDenominator = (Math.pow(secondComplex[0], 2) + Math.pow(secondComplex[1], 2));
+        double realPart = realNumerator/realDenominator;
+        System.out.println(realPart + "realPA");
+        double imagiNumerator = firstComplex[1]*secondComplex[0]-firstComplex[0]*secondComplex[1];
+        double imagiPart = imagiNumerator/realDenominator;
+        System.out.println(imagiPart + "imagiPA");
+        
+        return new Complex(realPart, imagiPart);
+    }
+    
+    
+    private void extractComponents(Complex first, Complex second){
+        double[] temp1 = {first.getRealP(),first.getImagiP()};
+        double[] temp2 = {second.getRealP(),second.getImagiP()};
+        firstComplex = temp1;
+        secondComplex = temp2;
     }
 }
--- test/ImaginaryCalcTest.java
+++ test/ImaginaryCalcTest.java
@@ -56,7 +56,7 @@ public class ImaginaryCalcTest {
         double imagiS = 320;
         Complex sComplex = new Complex(realS, imagiS);
         Complex complexSum = calc.sum(fComplex, sComplex);
-        System.out.println(complexSum.toString());
+        
         assertTrue(realP+realS == complexSum.getRealP() && imagiP+imagiS == complexSum.getImagiP());
     }
     
@@ -70,8 +70,39 @@ public class ImaginaryCalcTest {
         double imagiS = 320;
         Complex sComplex = new Complex(realS, imagiS);
         Complex complexSum = calc.mult(fComplex, sComplex);
-        System.out.println(complexSum.toString());
+        
         assertTrue(realP*realS-imagiP*imagiS == complexSum.getRealP());
         assertTrue(realP*imagiS+imagiP*realS == complexSum.getImagiP());
+    }
+    
+    @Test
+    public void substractTest(){
+        Calc calc = new Calc();
+        double realP = 5;
+        double imagiP = -10;
+        Complex fComplex = new Complex(realP, imagiP);
+        double realS = 50;
+        double imagiS = 320;
+        Complex sComplex = new Complex(realS, imagiS);
+        Complex complexSum = calc.substract(fComplex, sComplex);
+        
+        assertTrue(realP-realS == complexSum.getRealP() && imagiP-imagiS == complexSum.getImagiP());
+    }
+    
+     @Test
+    public void divisionTest(){
+        Calc calc = new Calc();
+        double realP = 3;
+        double imagiP = 2;
+        Complex fComplex = new Complex(realP, imagiP);
+        double realS = 1;
+        double imagiS = -2;
+        Complex sComplex = new Complex(realS, imagiS);
+        Complex divisionComplex = calc.division(fComplex, sComplex);
+        double realPart = (realP*realS+imagiP*imagiS)/(Math.pow(realS, 2)+Math.pow(imagiS, 2));
+        System.out.println(realPart);
+        double imagiPart = (imagiP*realS-imagiP*realS)/((Math.pow(realS, 2)+Math.pow(imagiS, 2))); 
+        System.out.println(imagiPart);
+        assertTrue(realPart == divisionComplex.getRealP() && imagiPart == divisionComplex.getImagiP());
     }
 }
