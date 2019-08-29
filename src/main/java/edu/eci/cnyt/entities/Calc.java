f7a26fe9940db1432e456a33710a177ce368f8c6
Daniel <rosalesdaniel350@gmail.com>
2019-08-29 06:10:58 -0400

    faltan dos funciones

--- src/main/java/edu/eci/cnyt/entities/Calc.java
+++ src/main/java/edu/eci/cnyt/entities/Calc.java
@@ -33,8 +33,6 @@ public class Calc {
         return new Complex(sumReal, sumImagi);
     }
 
-       
-
     /**
      * 
      * @param first
@@ -62,13 +60,13 @@ public class Calc {
         return new Complex(multReal, multImagi);
     }
 
-    public Complex constantMult(double cons,Complex c1){
-        double multReal = cons*c1.getRealP();
-        double multImagi = cons*c1.getImagiP();
-        return new Complex(multReal,multImagi);
+    public Complex constantMult(double cons, Complex c1) {
+        double multReal = cons * c1.getRealP();
+        double multImagi = cons * c1.getImagiP();
+        return new Complex(multReal, multImagi);
     }
 
-    public Complex[][] scalarMult(Complex scalar, Complex[][]c1){
+    public Complex[][] scalarMult(Complex scalar, Complex[][] c1) {
         Complex[][] scalarMult = new Complex[c1.length][c1[0].length];
         for (int i = 0; i < c1.length; i++) {
             for (int j = 0; j < c1[0].length; j++) {
@@ -96,17 +94,16 @@ public class Calc {
         return new Complex(Double.valueOf(df.format(realPart)), Double.valueOf(df.format(imagiPart)));
     }
 
-    public Complex[][] complexDivMatrix(Complex complex, Complex[][]matrix){
-        Complex[][]ans = new Complex[matrix.length][matrix[0].length];
+    public Complex[][] complexDivMatrix(Complex complex, Complex[][] matrix) {
+        Complex[][] ans = new Complex[matrix.length][matrix[0].length];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                ans[i][j] = this.division(matrix[i][j], complex); 
+                ans[i][j] = this.division(matrix[i][j], complex);
             }
         }
 
         return ans;
     }
-
 
     // ------------ Matrix zone--------------------
 
@@ -120,7 +117,7 @@ public class Calc {
         return ans;
     }
 
-    public Complex[][] mult(Complex[][] c1, Complex[][] c2){
+    public Complex[][] mult(Complex[][] c1, Complex[][] c2) {
         Complex[][] mult = new Complex[c1.length][c2[0].length];
         for (int i = 0; i < c1.length; i++) {
             for (int j = 0; j < c2[0].length; j++) {
@@ -146,26 +143,25 @@ public class Calc {
         return ans;
     }
 
-
     public Complex determinant(Complex[][] c1) {
-        
-        Complex ans = new Complex(0,0);
+
+        Complex ans = new Complex(0, 0);
         if (c1.length > 2) {
             for (int m = 0; m < c1.length; m++) {
-                Complex[][] subMatrix = subMatrix(m,0, c1);
+                Complex[][] subMatrix = subMatrix(m, 0, c1);
 
                 Complex constMult = this.constantMult(Math.pow(-1, m), determinant(subMatrix));
 
-                ans = this.sum(ans, this.mult(c1[m][0],constMult));
+                ans = this.sum(ans, this.mult(c1[m][0], constMult));
             }
             return ans;
-        }else{
+        } else {
             return determinant2x2(c1);
         }
     }
 
     public Complex determinant2x2(Complex[][] matrix) {
-        Complex ans = substract(mult(matrix[0][0], matrix[1][1]),mult(matrix[0][1], matrix[1][0]));
+        Complex ans = substract(mult(matrix[0][0], matrix[1][1]), mult(matrix[0][1], matrix[1][0]));
         return ans;
     }
 
@@ -175,31 +171,30 @@ public class Calc {
         for (int n = 0; n < c1.length; n++) {
             for (int m = 0; m < c1.length; m++) {
 
-                Complex [][] subMatrix =this.subMatrix(n, m, c1); 
+                Complex[][] subMatrix = this.subMatrix(n, m, c1);
                 adj[n][m] = determinant(subMatrix);
-                
+
             }
         }
 
         Complex[][] ans = new Complex[c1.length][c1[0].length];
         for (int i = 0; i < c1.length; i++) {
             for (int j = 0; j < c1[0].length; j++) {
-                adj[i][j] = this.constantMult(Math.pow(-1, i+j),adj[i][j]);
+                adj[i][j] = this.constantMult(Math.pow(-1, i + j), adj[i][j]);
             }
         }
         adj = this.transpose(adj);
         return adj;
     }
 
-
-    public Complex[][] inverse(Complex[][] c1){
+    public Complex[][] inverse(Complex[][] c1) {
         Complex determinant = this.determinant(c1);
         Complex[][] adjoint = this.adjunt(c1);
         Complex[][] inverse = this.complexDivMatrix(determinant, adjoint);
         return inverse;
     }
 
-    public Complex[][] conjugateMatrix(Complex[][] c1){
+    public Complex[][] conjugateMatrix(Complex[][] c1) {
         Complex[][] conjugateMatrix = new Complex[c1.length][c1[0].length];
         for (int i = 0; i < c1.length; i++) {
             for (int j = 0; j < c1[0].length; j++) {
@@ -209,28 +204,84 @@ public class Calc {
         return conjugateMatrix;
     }
 
-    private Complex[][] subMatrix(int noTouchI,int noTouchJ,Complex[][]c1){
-        //System.out.println(noTouch);
+    public Complex[][] subMatrix(int noTouchI, int noTouchJ, Complex[][] c1) {
+        // System.out.println(noTouch);
         Complex[][] mMatrix = new Complex[c1.length - 1][c1.length - 1];
         int i, j, p = 0;
-        for ( i = 0; i < c1.length; i++) {
+        for (i = 0; i < c1.length; i++) {
             int q = 0;
-            for ( j = 0; j < c1.length; j++) {
+            for (j = 0; j < c1.length; j++) {
                 if (i != noTouchI && j != noTouchJ) {
-                    //System.out.println(p+","+q);
+                    // System.out.println(p+","+q);
                     mMatrix[p][q] = c1[i][j];
-                    //System.out.print(mMatrix[p][q].toString()+",");
+                    // System.out.print(mMatrix[p][q].toString()+",");
                     q += 1;
                 }
             }
             if (i != noTouchI && j != noTouchJ)
                 p += 1;
-                //System.out.println();
+            // System.out.println();
         }
 
         return mMatrix;
     }
 
+    public Complex matrixNorm(Complex[][] c1){
+        Complex temp = new Complex(0,0);
+        for (int i = 0; i < c1.length; i++) {
+            for (int j = 0; j < c1[0].length; j++) {
+                Complex pow = mult(c1[i][j], c1[i][j]);
+                 temp = sum(temp,pow);
+            }
+        }
+         Complex ans = 
+         return null;
+     }
+
+    public Complex[][] accion(Complex[][] c1, Complex[][] v1) {
+
+        Complex[][] action = new Complex[v1.length][1];
+        Complex result;
+        for (int i = 0; i < v1.length; i++) {
+            result = new Complex();
+            for (int j = 0; j < c1[0].length; j++) {
+                result = sum(result, mult(c1[i][j], v1[0][j]));
+            }
+            action[i][0] = result;
+        }
+        return action;
+    }
+
+    public boolean isHermitia(Complex[][] c1) {
+        boolean hermitia = true;
+        Complex[][] adjunt = this.adjunt(c1);
+        hermitia = equalsMatrix(c1, adjunt);
+        return hermitia;
+    }
+
+    public boolean unitary(Complex[][] c1) {
+        boolean isUnitary = true;
+        Complex[][] transpose = this.transpose(c1);
+        Complex[][] ans = this.mult(c1, transpose);
+        Complex one = new Complex(1, 0);
+        for (int i = 0; i < ans.length && isUnitary; i++) {
+            if (!ans[i][i].equals(one))
+                isUnitary = false;
+
+        }
+
+        return isUnitary;
+    }
+
+    private boolean equalsMatrix(Complex[][] c1, Complex[][] c2) {
+        boolean equals = true;
+        for (int i = 0; i < c1.length && equals; i++) {
+            for (int j = 0; j < c1[0].length && equals; j++) {
+                equals = c1[i][j].equals(c2[i][j]);
+            }
+        }
+        return equals;
+    }
 
     private void extractComponents(Complex c1, Complex c2) {
         double[] temp1 = { c1.getRealP(), c1.getImagiP() };
@@ -239,11 +290,11 @@ public class Calc {
         secondComplex = temp2;
     }
 
-    private void printMatrix(Complex[][] matrix){
+    private void printMatrix(Complex[][] matrix) {
 
-        for (int i = 0; i < matrix.length;i++) {
-            for (int j = 0; j < matrix[0].length;j++) {
-                System.out.print(matrix[i][j].toString()+" ");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j].toString() + " ");
             }
             System.out.println();
         }
